7a624452f030af182c7c1ae273b8006da60080de
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-01 00:58:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,12 +12,10 @@ end
 
 def add_element_to_start_of_array(array, element)
 add_element_to_end_of_array.unshift("#{element}")
-
 end
 
 def remove_element_from_end_of_array(array)
 add_element_to_end_of_array.pop
-puts add_element_to_end_of_array.inspect
 end
 
 def remove_element_from_start_of_array(array)
