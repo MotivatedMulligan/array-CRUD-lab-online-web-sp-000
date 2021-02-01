334e390477d2cd9b9b748cd4ee55d1a233996e48
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-02-01 01:07:53 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,29 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-add_element_to_end_of_array.unshift("#{element}")
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-add_element_to_end_of_array.pop
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  add_element_to_end_of_array.shift
-puts add_element_to_end_of_array.inspect
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  add_element_to_end_of_array.
-puts add_element_to_end_of_array.inspect
+array.index()
 end
 
 def retrieve_first_element_from_array(array)
-  add_element_to_end_of_array.first
-puts add_element_to_end_of_array.inspect
+array.first
 end
 
 def retrieve_last_element_from_array(array)
-  add_element_to_end_of_array.last
-puts add_element_to_end_of_array.inspect
+array.last
 end
